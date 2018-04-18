5ae029c47513b107c66db15c72080f04f379a0ac
ashwani999 <ashwanisekhar999@gmail.com>
2018-04-18 17:53:40 +0530

    Add files via upload

---
+++ client/src/app/compi/compi.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompiComponent } from './compi.component';
+
+describe('CompiComponent', () => {
+  let component: CompiComponent;
+  let fixture: ComponentFixture<CompiComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CompiComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/cprog/cprog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cprog',
+  templateUrl: './cprog.component.html',
+  styleUrls: ['./cprog.component.css']
+})
+export class CprogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
